8af71c159dce2a6c84dc2321ee1d86a085541ebf
Lalit sharma <geekymonkcp742k@gmail.com>
2023-08-05 15:45:23 +0530

    Add TypeScript interfaces for application state

---
+++ utils/interfaces.ts
@@ -0,0 +1,42 @@
+export interface TagsProps {
+  tags: string[]
+}
+
+export interface TruncateProps {
+  text: string
+  startChars: number
+  endChars: number
+  maxLength: number
+}
+
+export interface AnswerProp {
+  id: number
+  qid: number
+  comment: string
+  owner: string
+  deleted: boolean
+  created: number
+}
+
+export interface QuestionProp {
+  id: number
+  title: string
+  description: string
+  owner: string
+  winner: string
+  paidout: boolean
+  deleted: boolean
+  updated: number
+  created: number
+  answers: number
+  tags: string[]
+  prize: number
+}
+
+export interface GlobalState {
+  wallet: string | null
+}
+
+export interface RootState {
+  globalStates: GlobalState
+}
