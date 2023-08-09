7add57e6014f15cbf9b0788f859e278666219559
Lalit sharma <geekymonkcp742k@gmail.com>
2023-08-09 11:49:08 +0530

    storing ideasData/questionData in redux store

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,27 @@ import Empty from '@/components/Empty'
 import Header from '@/components/Header'
 import Questions from '@/components/Questions'
 import { getQuestions } from '@/services/blockchain'
-import { QuestionProp } from '@/utils/interfaces'
+import { QuestionProp, RootState } from '@/utils/interfaces'
 import Head from 'next/head'
+import { useDispatch, useSelector } from 'react-redux'
+import { globalStates } from '../store/states/globalStates'
+import { globalActions } from '@/store/globalSlices'
+import { useEffect } from 'react'
 
-export default function Home({ questions }: { questions: QuestionProp[] }) {
+export default function Home({ questionsData }: { questionsData: QuestionProp[] }) {
+  // console.log(questionsData)
+  // getting questions from redux store
+  const dispatch = useDispatch()
+
+  const { setQuestions } = globalActions
+
+  const { questions } = useSelector((states: RootState) => states.globalStates)
+
+  // setting questions in redux store
+  useEffect(() => {
+    dispatch(setQuestions(questionsData))
+  }, [dispatch, setQuestions, questionsData])
+
   return (
     <div>
       <Head>
@@ -17,8 +34,8 @@ export default function Home({ questions }: { questions: QuestionProp[] }) {
 
       <main className="min-h-screen w-screen pb-20 radial-gradient">
         <Header />
-        <Banner />
-        {questions.length > 0 ? <Questions questions={questions} /> : <Empty />}
+        <Banner questions={questionsData} />
+        {questionsData.length > 0 ? <Questions questions={questionsData} /> : <Empty />}
         <AddQuestion />
       </main>
     </div>
@@ -28,6 +45,6 @@ export default function Home({ questions }: { questions: QuestionProp[] }) {
 export const getServerSideProps = async () => {
   const data: QuestionProp[] = await getQuestions()
   return {
-    props: { questions: JSON.parse(JSON.stringify(data)) },
+    props: { questionsData: JSON.parse(JSON.stringify(data)) },
   }
 }
