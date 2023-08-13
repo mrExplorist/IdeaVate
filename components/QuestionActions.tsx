42b65bafddc2df7a54138b8c6be053372eec6540
Lalit sharma <geekymonkcp742k@gmail.com>
2023-08-13 17:58:20 +0530

    DeleteIdeaModal reduxActions

--- components/QuestionActions.tsx
+++ components/QuestionActions.tsx
@@ -9,7 +9,7 @@ import { globalActions } from '@/store/globalSlices'
 
 const QuestionActions = () => {
   const dispatch = useDispatch()
-  const { setUpdateQuestionModal } = globalActions
+  const { setUpdateQuestionModal, setDeleteQuestionModal } = globalActions
   return (
     <Menu as="div" className="inline-block text-left text-[#BBBBBB]">
       <Menu.Button
@@ -23,8 +23,8 @@ const QuestionActions = () => {
       </Menu.Button>
       <Menu.Items
         className="absolute right-0 mr-2 mt-2 w-56 origin-top-right
-          divide-y divide-[#BBBBBB] rounded-md bg-[#16112E] shadow-md 
-          ing-1 ring-black ring-opacity-5 focus:outline-none shadow-pink-500 "
+           divide-[#BBBBBB] rounded-md bg-[#16112E] shadow-md 
+          ring-1 ring-black ring-opacity-5 focus:outline-none shadow-pink-900 "
       >
         <Menu.Item>
           {({ active }) => (
@@ -34,8 +34,8 @@ const QuestionActions = () => {
               } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
               onClick={() => dispatch(setUpdateQuestionModal('scale-100'))}
             >
-              <FiEdit size={17} />
-              <span>Edit</span>
+              <FiEdit size={16} />
+              <span> Edit</span>
             </button>
           )}
         </Menu.Item>
@@ -45,7 +45,7 @@ const QuestionActions = () => {
               className={`flex justify-start items-center space-x-1 ${
                 active ? 'bg-red-500' : 'text-red-500'
               } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-              // onClick={() => dispatch(setQuestionDeleteModal('scale-100'))}
+              onClick={() => dispatch(setDeleteQuestionModal('scale-100'))}
             >
               <BsTrash3 size={17} />
               <span>Delete</span>
