8f9c05e25f7be291f6e2217a67f75cc3f6788d7a
Lalit sharma <geekymonkcp742k@gmail.com>
2023-08-13 13:15:35 +0530

    added GlobalStates and Actions for performing actions on idea

--- store/actions/globalActions.ts
+++ store/actions/globalActions.ts
@@ -5,4 +5,16 @@ export const globalActions = {
   setWallet: (state: GlobalState, action: PayloadAction<string>) => {
     state.wallet = action.payload
   },
+  setQuestions: (state: GlobalState, action: PayloadAction<any>) => {
+    state.questions = action.payload
+  },
+  setQuestion: (state: GlobalState, action: PayloadAction<any>) => {
+    state.question = action.payload
+  },
+  setAddQuestionModal: (state: GlobalState, action: PayloadAction<string>) => {
+    state.addQuestionModal = action.payload
+  },
+  setUpdateQuestionModal: (state: GlobalState, action: PayloadAction<string>) => {
+    state.updateQuestionModal = action.payload
+  },
 }
--- store/states/globalStates.ts
+++ store/states/globalStates.ts
@@ -2,4 +2,8 @@ import { GlobalState } from '@/utils/interfaces'
 
 export const globalStates: GlobalState = {
   wallet: '',
+  questions: [],
+  question: null,
+  addQuestionModal: 'scale-0',
+  updateQuestionModal: 'scale-0',
 }
